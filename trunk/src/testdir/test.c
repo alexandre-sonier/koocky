6bacc891408723c0a6b7ae9f827d9b95685c5c5c
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-10 14:00:05 +0000

    jonction du @import / @module / AstToFile

---
+++ trunk/src/testdir/test.c
@@ -0,0 +1,12 @@
+#include "bla.h"
+signed int _4bobo_variable_signed_int_1a;
+char _4bobo_variable_signed_char_1a;
+signed int main ()
+{
+	signed int c  = 84;
+	_4bobo_variable_signed_int_1a = 42;
+	_4bobo_variable_signed_char_1a = 'z';
+	c = _4bobo_variable_signed_int_1a;
+	a = a + 1;
+}
+
