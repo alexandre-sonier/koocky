f398d63e06a436d2a1b17664e5fda29f11a63317
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-19 15:03:16 +0000

    finalization of the translation

---
+++ trunk/src/test.c
@@ -0,0 +1,14 @@
+signed int _4bobo_variable_signed_int_1a;
+signed int 	** _4bobo_variable_signed_int_point_point_1a;
+float _4bobo_variable_float_1a;
+char _4bobo_variable_signed_char_1a;
+char _4bobo_variable_signed_char_1a;
+void _4bobo_function_void_1f_param_void (void);
+signed int bla (signed int, signed int, char, signed int 	**);
+signed int main ()
+{
+	signed int i;
+	bla(_4bobo_variable_signed_int_1a, _4bobo_variable_signed_int_point_point_1a, _4bobo_variable_signed_int_1a + 4, _4bobo_variable_signed_int_1a + i);
+	i = bla(_4bobo_variable_signed_int_1a, _4bobo_variable_signed_int_point_point_1a, _4bobo_variable_signed_int_1a + 4, _4bobo_variable_signed_int_1a + i);
+}
+
