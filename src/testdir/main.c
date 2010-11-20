076fb698cff4235eb5903588c6638a33de2813fc
Gbarry.epitech@gmail.com <Gbarry.epitech@gmail.com@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-20 17:06:06 +0000

    Ajout du generateur de Makefile

--- src/testdir/main.c
+++ src/testdir/main.c
@@ -1,15 +1,1 @@
 #include "a.h"
-signed int main (void)
-{
-	signed int a;
-	char c;
-	_4bobo_variable_signed_int_1a = 42;
-	a = _4bobo_variable_signed_int_1a;
-	_4bobo_variable_signed_int_1f = a;
-	a = _4bobo_variable_signed_int_1a + _4bobo_variable_signed_int_1f + _4bobo_variable_signed_int_1a + 42 + 42 + 42 + a + a + a - _4bobo_variable_signed_int_1a * _4bobo_variable_signed_int_1f;
-	_4bobo_variable_signed_char_1a = 'c';
-	c = 'b';
-	_4bobo_variable_signed_char_1a = c;
-	_4bobo_variable_signed_int_1f = _4bobo_variable_signed_int_1a;
-}
-
