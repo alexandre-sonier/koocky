d1f0e2b4a68ee4ddad545706850f9236bd1b5db4
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-21 17:30:46 +0000

    updating cws main file

--- src/testdir/a.c
+++ src/testdir/a.c
@@ -1,7 +1,7 @@
 #include "a.h"
 void _4bobo_function_void_1f_param_signed_int_signed_int (signed int a, signed int b)
 {
-	printf("%d + %d = %d", a, b, a + b);
+	printf("%d + %d = %d\n", a, b, a + b);
 }
 
 signed int _4bobo_variable_signed_int_1a  = 2;
