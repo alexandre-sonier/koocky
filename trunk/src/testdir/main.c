b28e6520b209e27e4618a89187839f74f989519b
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-21 17:28:44 +0000

    sending testfiles

--- trunk/src/testdir/main.c
+++ trunk/src/testdir/main.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "a.h"
-signed int main (void)
+signed int main ()
 {
 	signed int a  = 1;
-	printf("%d + %d = %d\n", a, _4bobo_variable_signed_int_1b, a + _4bobo_variable_signed_int_1b);
-	_4bobo_function_void_1f_param_signed_int_signed_int(a, _4bobo_variable_signed_int_1b);
+	_4bobo_function_void_1f_param_signed_int_signed_int(_4bobo_variable_signed_int_1a, a);
+	return (0);
 }
 
