6eaf3a5e6dbbd17337a564c1fcaec3feaed4dae6
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-23 15:31:24 +0000

    including object class

--- tests/b.c
+++ tests/b.c
@@ -5,3 +5,8 @@ void _4bobo_function_void_1f_param_signed_int_signed_int (signed int a, signed i
 	printf("%d + %d = %d\n", a, b, a + b);
 }
 
+signed int _4bobo_function_signed_int_1a_param_signed_int (signed int b)
+{
+	return (b + 1);
+}
+
