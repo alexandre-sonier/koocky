3bdb927e50678df8f1b020d445e97ca7b334b1b2
jeremy.mengoni <jeremy.mengoni@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-24 12:48:57 +0000

    update of the var call

--- tests/main.c
+++ tests/main.c
@@ -1,18 +1,2 @@
 #include "a.h"
 #include "b.h"
-
-signed int main ()
-{
-	signed int a  = 1;
-	test tutu;
-	tutu.M_4test_variable_signed_int_1a = 2;
-	_4bobo_function_void_1f_param_signed_int_signed_int(_4bobo_variable_signed_int_1a, a);
-	printf("%d + %d = %d\n", _4bobo_variable_signed_int_1a, a, a + _4bobo_variable_signed_int_1a);
-	if (_4test_variable_signed_int_1b > a)
-		while (_4test_variable_signed_int_1b > a + 80)
-		if (_4test_variable_signed_int_1b < 100)
-		_4test_variable_signed_int_1b += 1;
-	printf("%d\n", _4bobo_function_signed_int_1a_param_signed_int(10));
-	return 0;
-}
-
