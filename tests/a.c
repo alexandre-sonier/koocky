a0160f986531ff4aca20d52cf1b11fe389b9526b
alexandre.sonier <alexandre.sonier@0f8b9722-efb3-d7d5-d457-b63b6537b70e>
2010-11-27 19:15:24 +0000

    Beta 2

--- tests/a.c
+++
@@ -1,9 +0,0 @@
-#include "Object.ko"
-#include "a.h"
-signed int _4test_variable_signed_int_1b  = 20;
-signed int M_4test_function_signed_int_1f_param_signed_int (struct _class_test_ 	* self, signed int a)
-{
-	return a;
-}
-
-struct _vtable_test_ vttest  = {};
--- tests/b.h
+++
@@ -1,5 +0,0 @@
-#include "Object.ko"
-#include "/usr/include/stdio.h"
-extern signed int _4bobo_variable_signed_int_1a;
-signed int _4bobo_function_signed_int_1a_param_signed_int (signed int);
-void _4bobo_function_void_1f_param_signed_int_signed_int (signed int, signed int);
